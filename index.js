41693270ca84a0d7f84336ed831e1d63f0f245e7
kilik42 <marvinevins@yahoo.com>
2026-06-08 18:47:58 -0500

    google webscraper

---
+++ index.js
@@ -0,0 +1,24 @@
+const {getJson} = require('serpapi');
+require('dotenv').config();
+
+getJson({
+    engine: "google",
+    q: "Coffee",
+    location: "Austin, Texas, United States",
+    google_domain: "google.com",
+    hl: "en",
+    api_key: process.env.SERP_API_KEY
+})
+
+// const search = async () => {
+//   const params = {
+//     q: "Coffee", // query
+//     api_key: process.env.SERP_API_KEY // API key from .env file
+//   };
+
+//   const response = await getJson("https://serpapi.com/search", params);
+//   console.log(response);
+
+// };
+
+// search();
